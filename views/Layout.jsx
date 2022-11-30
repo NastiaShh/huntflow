7583273a53370b536a3db725f0bb6ad7831e8a5b
Anastasiia Shamrai <sky.blush@ya.ru>
2022-11-30 18:16:41 +0300

    add Layout component

---
+++ views/Layout.jsx
@@ -0,0 +1,28 @@
+const React = require('react');
+const NavBar = require('./NavBar');
+
+function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Huntflow</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        />
+        {/* <link rel="stylesheet" type="text/css" href="/css/style.css" /> */}
+      </head>
+      <body>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
+
+module.exports = Layout;
