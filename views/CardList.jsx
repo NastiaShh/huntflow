84a305be422a08ef6241a2c091546f0731162117
Anastasiia Shamrai <sky.blush@ya.ru>
2022-12-03 13:41:24 +0300

    update styles

--- views/CardList.jsx
+++ views/CardList.jsx
@@ -1,14 +1,13 @@
 const React = require('react');
 const Card = require('./Card');
-const path = require('path');
 
 module.exports = function CardList({ candidates }) {
   return (
     <div className="container card-list">
       <div className="card" id="empty-card">
-        <img src='/img/20180125_001_1_.jpg' className="card-img-top" alt="Фотография кандидата" />
-        <div className="card-body">
-          <a href={`/api/candidates/`} className="btn btn-info btn-new-candidate">
+        <img src="/img/20180125_001_1_.jpg" className="card-img-top" alt="Новый кандидат" style={{opacity: "0.5"}}/>
+        <div className="card-body empty-card-body">
+          <a href="/api/candidates/" className="btn btn-info" >
             Добавить нового кандидата
           </a>
         </div>
